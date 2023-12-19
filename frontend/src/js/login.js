f8b23d275352f9f1383fab3ab9ac28e0ab90b156
socsig2210 <socsig2210@gmail.com>
2023-12-19 18:02:23 +0200

    login, register flow

--- frontend/src/js/login.js
+++ frontend/src/js/login.js
@@ -17,7 +17,7 @@ function decodeJwt(jwtToken) {
     const getUsernameLogin = document.getElementById("login-username").value;
     //get user password
     const getPasswordLogin = document.getElementById("login-password").value;
-  
+
     try {
       var myHeaders = new Headers();
       myHeaders.append("Content-Type", "application/x-www-form-urlencoded");
@@ -25,7 +25,7 @@ function decodeJwt(jwtToken) {
       var urlencoded = new URLSearchParams();
       urlencoded.append("username", getUsernameLogin);
       urlencoded.append("password", getPasswordLogin);
-      urlencoded.append("client_id", "client-front");
+      urlencoded.append("client_id", "frontend-app");
       urlencoded.append("client_secret", "6PP1lYj2D77DLWaiiiZCftP91QbRnBrX");
       urlencoded.append("grant_type", "password");
   
@@ -37,15 +37,26 @@ function decodeJwt(jwtToken) {
       };
   
       const response = await fetch("http://localhost:8182/auth/realms/e-shop/protocol/openid-connect/token", requestOptions)
+      
       if(response.ok){
         const login = await response.json()
         const token = login.access_token
+
+        // console.log(JSON.stringify(login))
         
         //store in localstorage username, email, role (customer, seller) and refresh_token
         const decodeToken = await decodeJwt(token)
-        localStorage.setItem("username", decodeToken.preferred_username)
+
+        // localStorage.setItem("username", decodeToken.preferred_username)
+        // localStorage.setItem("email", decodeToken.email)
+        localStorage.setItem("role", decodeToken.realm_access.roles[0])
+        localStorage.setItem("refresh_token", login.refresh_token)
+
+        console.log(localStorage)
         //clear localStorage
         // localStorage.clear()
+
+        location.href = "http://localhost:8000/home.html"
       }else{
         const err = await response.json()
         console.log(err) 
@@ -66,7 +77,7 @@ function decodeJwt(jwtToken) {
     const getUsername = document.getElementById("register-username").value;
     const getEmail = document.getElementById("register-email").value;
     const getPassword = document.getElementById("register-password").value;
-    const getRole = document.getElementById("register-user-type").value;
+    const getRole = document.getElementById("user-role").value;
   
     try {
       var myHeaders = new Headers();
@@ -94,13 +105,13 @@ function decodeJwt(jwtToken) {
         var myHeaders = new Headers();
         myHeaders.append("Content-Type", "application/json");
         myHeaders.append("Authorization", "Bearer "+token);
-  
+
         var raw = JSON.stringify({
           "email": getEmail,
-          "enabled": "true",
+          "enabled": true,
           "username": getUsername,
           "attributes": {
-            "client_id": "client-front"
+            "client_id": "frontend-app"
           },
           "groups": [
             getRole
@@ -114,26 +125,26 @@ function decodeJwt(jwtToken) {
           ]
         });
   
-      var registerOptions = {
-        method: 'POST',
-        headers: myHeaders,
-        body: raw,
-        redirect: 'follow'
-      };
-  
-      const registerUser =  await fetch("http://localhost:8182/auth/admin/realms/e-shop/users", registerOptions)
-      
-      if(registerUser.ok){
-        alert('register user is ok')
-  
-        setTimeout(()=>{
-          window.location.href = "http://localhost:5500/Cloud_lessons/lesson4/frontend/src/login.html"
-        },2000)
+        var registerOptions = {
+          method: 'POST',
+          headers: myHeaders,
+          body: raw,
+          redirect: 'follow'
+        };
+    
+        const registerUser =  await fetch("http://localhost:8182/auth/admin/realms/e-shop/users", registerOptions)
         
-      }else{
-        const err = await registerUser.json()
-        console.log(err)
-      }
+        if(registerUser.ok){
+          alert('register user is ok')
+    
+          setTimeout(()=>{
+            window.location.href = "http://localhost:8000"
+          },2000)
+          
+        }else{
+          const err = await registerUser.json()
+          console.log(err)
+        }
   
       }else{
         const err = await first_response.json();
@@ -146,14 +157,6 @@ function decodeJwt(jwtToken) {
   
     return false
   }
-
-
-
-
-
-
-
-
 
 
 
